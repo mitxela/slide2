0b5fa97a3bd25f71e141adb4ec76a598af458a38
mitxela <mitxela@mitxela.com>
2022-05-31 17:02:25 +0100

    calibration scripts

---
+++ cal.py
@@ -0,0 +1,36 @@
+#!/bin/env python3
+
+import serial
+s = serial.Serial('/dev/ttyUSB1', baudrate=31250)
+
+# valves, arms, fan motors
+def w( v1,v2,v3,v4, a1,a2,a3,a4, f1,f2,f3,f4):
+  msg = [0xFF, 0xAA,
+    v1>>8,v1&0xFF, v2>>8,v2&0xFF, v3>>8,v3&0xFF, v4>>8,v4&0xFF,
+    a1,a2,a3,a4,
+    f1>>8,f1&0xFF, f2>>8,f2&0xFF, f3>>8,f3&0xFF, f4>>8,f4&0xFF ]
+
+  s.write(bytearray( msg + [~sum(msg)&0xFF] ))
+
+vopen = [1250,1200,1310,1150]
+vclosed = [1150,1100,1220,1050]
+
+def setpos(whistle, angle, speed=-1):
+  if speed==-1:
+#    speed = int(600+(angle/250)*350)
+    speed = int( 640 + ((angle/250)**1.5)*450 )
+  if whistle==1:
+    w( vopen[0],vclosed[1],vclosed[2],vclosed[3], angle,45,45,45, speed,0,0,0)
+  elif whistle==2:
+    w( vclosed[0],vopen[1],vclosed[2],vclosed[3], 45,angle,45,45, 0,speed,0,0)
+  elif whistle==3:
+    w( vclosed[0],vclosed[1],vopen[2],vclosed[3], 45,45,angle,45, 0,0,speed,0)
+  elif whistle==4:
+    w( vclosed[0],vclosed[1],vclosed[2],vopen[3], 45,45,45,angle, 0,0,0,speed)
+
+def off():
+  w( 1150,1100,1220,1050,  45,45,45,45, 0,0,0,0)
+
+if __name__ == "__main__":
+  w( 0,0,0,0,  90,90,90,90, 0,0,0,0)
+  #w(0,1150,0,0,145,145,145,145,0,0,0,1100)
---
+++ sweep.py
@@ -0,0 +1,126 @@
+#!/bin/env python3
+
+from cal import w, setpos, off
+from time import sleep
+
+import pyaudio
+import numpy as np
+import matplotlib.pyplot as plt
+
+CHUNK = 8192
+FORMAT = pyaudio.paFloat32
+CHANNELS = 2
+RATE = 48000
+RECORD_SECONDS = 1
+
+p = pyaudio.PyAudio()
+
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+devID = -1
+
+for i in range(0, numdevices):
+  if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+    #print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+    if p.get_device_info_by_host_api_device_index(0, i).get('name') == "RØDE VideoMic NTG: USB Audio (hw:1,0)":
+      devID = i
+
+if devID==-1:
+  print("Can't find device")
+  exit()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                input_device_index=devID,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+def pitch(d):
+  dc = sum(d)/len(d)
+  d = d - dc
+
+  #[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
+  t = np.linspace(-10,10,30)
+  gauss = np.exp(-0.1*t**2)
+  gauss /= np.trapz(gauss)
+  # plt.plot(gauss)
+  # plt.show()
+
+  d=np.convolve(d, gauss)
+
+  #plt.plot(d)
+  #plt.show()
+
+  sign = np.sign(d[0])
+
+  crossings=[]
+  for i,s in enumerate(d):
+    if (np.sign(s)!=sign):
+      crossings.append(i)
+      sign = np.sign(s)
+
+  periods = np.diff(crossings)
+  return RATE*len(periods)/sum(periods)
+
+
+def mic_pitch():
+  data=[]
+  for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+      data = np.append(data,np.frombuffer(stream.read(CHUNK, exception_on_overflow=False), dtype=np.float32))
+
+  return pitch(data)
+
+stream.read(CHUNK, exception_on_overflow=False) # first chunk is junk
+
+
+
+def delaySec(sec):
+  len = round(sec*RATE/CHUNK)
+  for i in range(len):
+    stream.read(CHUNK, exception_on_overflow=False)
+
+whistle=3
+
+setpos(whistle,0,1000)
+delaySec(0.5)
+setpos(whistle,0)
+delaySec(4)
+print("* ready")
+#print(mic_pitch())
+
+
+
+forwards={}
+backwards={}
+
+step=10
+
+print("Forwards")
+for i in range(0, 250+step,step):
+  setpos(whistle,i)
+  delaySec(1)
+  forwards[i]=mic_pitch()
+  print(i, forwards[i])
+
+print("Backwards")
+for i in range(250, -step,-step):
+  setpos(whistle,i)
+  delaySec(1)
+  backwards[i]=mic_pitch()
+  print(i, backwards[i])
+
+off()
+
+print("Position Forwards Backwards Average")
+for i in range(0, 250+step,step):
+  print(i, forwards[i], backwards[i], (forwards[i]+backwards[i])/2.0)
+
+
+
+
+stream.stop_stream()
+stream.close()
+p.terminate()
